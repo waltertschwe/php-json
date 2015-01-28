ab17a3f6fbfcef476a658d3ca2c9f973adfba1d2
Walter Schweitzer <wschweitzer00@gmail.com>
2015-01-27 21:58:57 -0500

    initial check in

---
+++ Customer.php
@@ -0,0 +1,58 @@
+<?php
+
+class Customer {
+	
+	
+	public function _construct() {
+		
+	}
+	
+	public function setFirstName($firstName) {
+		$this->firstName = $firstName;		
+		return $this;
+	}
+	
+	public function getFirstName() {
+		return $this->firstName;
+	}
+	
+	public function setLastName($lastName) {
+		$this->lastName = $lastName;
+		return $this;
+	}
+	
+	public function getLastName() {
+		return $this->lastName;
+	}
+	
+	public function setEmail($email) {
+		//$results = array();
+		//foreach($email as $key => $value) {
+		//	$results[$key] = $value;
+		//}
+		$this->email[] = $email;
+		return $this;
+	}
+	
+	public function getEmail() {
+		return $this->email;
+	}
+	
+	public function setPhone($phone) {
+		 $this->phone[] = $phone;
+		 return $this;
+	}
+	
+	public function getPhone() {
+		return $this->phone;
+	}
+	
+	public function setAddress($address) {
+		$this->address[] = $address;
+	}
+	
+	public function getAddress() {
+		return $this->address;
+	}
+		
+}
